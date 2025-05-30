e48216363cf31999cbcb8fa5cc24f6929e686143
Vitor-Piress <vitorspires25@gmail.com>
2025-05-30 19:19:32 -0300

    Front-end: Home grid fixed / Back-end init

--- front-end/src/pages/home.jsx
+++ front-end/src/pages/home.jsx
@@ -4,7 +4,7 @@ import Item from "../components/Item";
 const home = () => {
   return (
     <section>
-      <div className="mx-auto grid max-w-7xl grid-cols-[repeat(auto-fit,minmax(225px,1fr))] gap-8 p-8">
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(225px,1fr))] gap-8 p-8">
         <Item />
         <Item />
         <Item />
